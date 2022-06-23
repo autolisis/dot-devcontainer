3244f1d50f0347a1c4b6bbe032001f833453c7e4
reisub0 <reisub0@gmail.com>
2022-06-23 15:43:17 +0200

    Initial commit

---
+++ .config/fish/conf.d/01-bootstrap.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME
+    or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+replay source $HOME/.bash_profile
+
+# Set up Pyenv
+if command -v pyenv 1>/dev/null 2>&1
+    pyenv init - | source
+end
+# cd $HOME
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
+"$dot" checkout -f
+
