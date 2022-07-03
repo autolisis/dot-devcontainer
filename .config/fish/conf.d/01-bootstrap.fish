33c3c8e722c34e179643654a3d9cd67ab4af23c5
reisub0 <reisub0@gmail.com>
2022-07-03 15:00:02 +0200

    Tries to fix fisher
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/01-bootstrap.fish
+++ .config/fish/conf.d/01-bootstrap.fish
@@ -4,7 +4,7 @@ if not functions -q fisher
     or set XDG_CONFIG_HOME ~/.config
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     # curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c "fisher update"
+    # fish -c "fisher update"
 end
 # Set up Pyenv
 if command -v pyenv 1>/dev/null 2>&1
