77baea205dd85b4fd4910461a27f80693ff80ad3
reisub0 <reisub0@gmail.com>
2022-07-03 19:18:29 +0200

    Tries to fix fisher
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 git clone --bare https://github.com/reisub0/dot-devcontainer ~/.dot
 git --git-dir=$HOME/.dot --work-tree=$HOME checkout -f
 
-fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update'
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
+git --git-dir=$HOME/.dot --work-tree=$HOME checkout -f
+fish -c 'fisher update'
 
