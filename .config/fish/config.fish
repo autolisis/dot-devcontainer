e23c882a045c85dc7ff576cb65a7a0da5d4774a5
reisub0 <reisub0@gmail.com>
2022-09-03 21:18:05 +0200

    Mambaforge doesnt seem stable yet
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,6 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/g/.pyenv/versions/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+eval /home/g/.pyenv/versions/miniforge3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
