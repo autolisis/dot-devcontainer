5860446fc588cc52fbc0f20610451ae841bac8bc
reisub0 <reisub0@gmail.com>
2022-09-03 22:20:18 +0200

    Failed conda/mamba experiment, reverting

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/g/.pyenv/versions/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
